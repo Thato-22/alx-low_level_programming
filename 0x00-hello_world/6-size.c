0c4b5c0d434adc03cfd77b5088198b99a0d57436
Thato-22 <jessiemotsepe95@gmail.com>
2023-06-15 20:42:17 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio>
+/**
+ * main - Entry point 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print("Size of char: %lu byte(s)\n", sizeof(char);
+	print("Size of int: %lu byte(s)\n", sizeof(int);
+	print("Size of long int: %lu byte(s)\n", sizeof(long int);
+	print("Size of long long int: %lu byte(s)\n", sizeof(long long int);
+	print("Size of float: %lu byte(s)\n", sizeof(float);
+	return (0);
+}
