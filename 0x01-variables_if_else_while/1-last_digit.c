ae1536a8f024873167bac406cea854e161094145
Thato-22 <jessiemotsepe95@gmail.com>
2023-06-19 02:49:55 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>	
-/**	
- * main - Entry point	
- * Return: Always return 0 (Success)	
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always return 0 (Success)
 */
 int main(void)
 {
